ebadf704985cc9750fa0f7e188f832897a002791
MaCheng <1256144040@qq.com>
2016-07-20 10:35:08 +0800

    Signed-off-by: MaCheng <1256144040@qq.com>

---
+++ src/com/suda/weixin/LoginActivity.java
@@ -0,0 +1,68 @@
+package com.suda.weixin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+        
+        EditText userNameText = (EditText) findViewById(R.id.textName);
+        userNameText.setCompoundDrawablePadding(20);
+        EditText passwordText = (EditText) findViewById(R.id.textPassword);
+        passwordText.setCompoundDrawablePadding(20);
+        
+        Button button = (Button) findViewById(R.id.btnEnter);
+        
+        /*s*/
+        /*++++++++++++++++++++++++++*/
+       /* proBar.setVisibility(View.GONE);*/
+        ProgressBar proLogin = (ProgressBar) findViewById(R.id.progressBar1);
+        proLogin.setVisibility(View.GONE);
+        
+        button.setOnClickListener(new OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+				startActivity(intent);
+			}
+		});
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
